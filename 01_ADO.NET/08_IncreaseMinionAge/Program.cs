60ba8e38f4d6d98bfbc240f93447e80b3ca03d14
GLidia <74815962+GLidia@users.noreply.github.com>
2022-04-28 15:11:56 +0300

    Added problems 8-9

---
+++ 01_ADO.NET/08_IncreaseMinionAge/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+
+namespace _08_IncreaseMinionAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] minionIds = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            string connectionString = "SERVER=.\\SQLExpress;Database=MinionsDB;Integrated Security=true;Encrypt=false";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            using (connection)
+            {
+                connection.Open();
+
+                string incrementAgeAndCapitalizeFirstLetterForMinions = "UPDATE Minions " +
+                    "SET Name = UPPER(LEFT(Name, 1)) + LOWER(SUBSTRING(Name, 2, LEN(Name)))," +
+                    "Age += 1" +
+                    "WHERE Id = @minionId";
+                
+                foreach (int id in minionIds)
+                {
+                    SqlCommand command1 = new SqlCommand(incrementAgeAndCapitalizeFirstLetterForMinions, connection);
+                    command1.Parameters.AddWithValue("@minionId", id);
+                    command1.ExecuteNonQuery();
+                }
+
+                string getNameAndAgeFromMinionsQuery = "SELECT Name, Age FROM Minions";
+                SqlCommand command2 = new SqlCommand(getNameAndAgeFromMinionsQuery, connection);
+                SqlDataReader reader = command2.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["Name"] as string + " " + reader["Age"] as string);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ 01_ADO.NET/09_IncreaseAgeStoredProcedure/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _09_IncreaseAgeStoredProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //In Management Studio:
+            //CREATE PROC usp_GetOlder(@minionId INT)
+            //AS
+            //UPDATE Minions
+            //SET Age += 1
+            //WHERE Id = @minionId
+            //GO
+
+            int id = int.Parse(Console.ReadLine());
+
+            string connectionString = "SERVER=.\\SQLExpress;Database=MinionsDB;Integrated Security=true;Encrypt=false";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand command1 = new SqlCommand("EXEC usp_GetOlder @minionId", connection);
+                command1.Parameters.AddWithValue("@minionId", id);
+                command1.ExecuteNonQuery();
+                
+                SqlCommand command2 = new SqlCommand("SELECT Name, Age FROM Minions WHERE Id = @minionId", connection);
+                command2.Parameters.AddWithValue("@minionId", id);
+                SqlDataReader reader = command2.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["Age"]} years old");
+                    }
+                }
+            }
+        }
+    }
+}
