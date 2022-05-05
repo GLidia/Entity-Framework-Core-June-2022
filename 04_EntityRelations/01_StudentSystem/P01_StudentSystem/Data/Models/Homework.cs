29e6c912d5f3badc0608c98548716a220a594bba
GLidia <74815962+GLidia@users.noreply.github.com>
2022-05-05 11:33:29 +0300

    Added problem 1 solution

---
+++ 04_EntityRelations/01_StudentSystem/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        public Homework(){ }
+
+        public int HomeworkId { get; set; }
+        public string Content { get; set; }
+        public ContentType ContentType { get; set; }
+        public DateTime SubmissionTime { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+
+    public enum ContentType
+    {
+        Application,
+        Pdf,
+        Zip
+    }
+}
---
+++ 04_EntityRelations/01_StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,61 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using P01_StudentSystem.Data.Models;
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext() { }
+        public StudentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.//SqlExpress;Database=StudentSystem;Integrated Security=True");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Student>()
+                .Property(s => s.PhoneNumber)
+                .IsUnicode(false);
+
+            builder.Entity<Resource>()
+                .Property(r => r.Url)
+                .IsUnicode(false);
+
+            builder.Entity<Homework>()
+                .Property(h => h.Content)
+                .IsUnicode(false);
+
+            builder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            builder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.CourseEnrollments)
+                .HasForeignKey(sc => sc.StudentId);
+
+            builder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentsEnrolled)
+                .HasForeignKey(sc => sc.CourseId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
