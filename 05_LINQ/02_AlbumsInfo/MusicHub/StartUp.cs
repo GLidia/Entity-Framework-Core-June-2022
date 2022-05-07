65029ea9bedbf74e5f1a1f6bf02c70a0fabeaad7
GLidia <74815962+GLidia@users.noreply.github.com>
2022-05-07 10:41:02 +0300

    Added problem 2 solution

---
+++ 05_LINQ/02_AlbumsInfo/MusicHub/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            string result = ExportAlbumsInfo(context, 9);
+            Console.WriteLine(result);
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var albums = context.Albums.Where(x => x.ProducerId == producerId)
+                .Select(x => new { AlbumName = x.Name, x.ReleaseDate, ProducerName = x.Producer.Name, x.Songs, AlbumPrice = x.Price })
+                .ToList();
+
+            albums = albums.OrderByDescending(x => x.AlbumPrice).ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var album in albums)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}");
+                sb.AppendLine($"-ReleaseDate: {album.ReleaseDate.ToString("MM/dd/yyyy")}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                sb.AppendLine("-Songs:");
+
+                int count = 0;
+                foreach (var song in album.Songs.OrderByDescending(x => x.Name).ThenBy(x => x.Writer))
+                {
+                    count++;
+                    sb.AppendLine($"---#{count}");
+                    sb.AppendLine($"---SongName: {song.Name}");
+                    sb.AppendLine($"---Price: {song.Price:F2}");
+                    sb.AppendLine($"---Writer: {song.Writer.Name}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:F2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
